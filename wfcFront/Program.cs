b743c861bf1d93ff9f12206f70055e453e9d765c
Andriy <kaminskyy.andriy@gmail.com>
2020-08-13 11:22:15 +0300

    Project structure

---
+++ wfcFront/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace wfcFront
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ wfcLib/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace wfcLib
+{
+    public class Class1
+    {
+    }
+}

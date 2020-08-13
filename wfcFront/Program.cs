f9381c590fc13eece740be0bbc025c42110b8b1c
Andriy <kaminskyy.andriy@gmail.com>
2020-08-13 13:11:52 +0300

    Added Electron.Net

--- wfcFront/Program.cs
+++ wfcFront/Program.cs
@@ -1,12 +1,28 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ElectronNET.API;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace wfcFront
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            CreateHostBuilder(args).Build().Run();
         }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseElectron(args);
+                webBuilder.UseStartup<Startup>();
+            });
     }
 }
